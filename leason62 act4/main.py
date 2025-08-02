5d7157fa73d5512c63bbda659a484b74c9266267
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-08-02 20:48:07 +0530

    DCDACZSDVczsdvzsd

---
+++ leason62 act4/main.py
@@ -0,0 +1,6 @@
+import datetime
+current_time = datetime.datetime.now()
+print("This now at greenwich merdian is :", end = "")
+print("current_time")
+import calendar
+print("\n", calendar.calendar(2021))
