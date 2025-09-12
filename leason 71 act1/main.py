2bf3fc498776e95f58185aa030b9be5c478f4708
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-09-12 20:24:38 +0530

    rgszsrggggggggg

---
+++ leason 71 act1/main.py
@@ -0,0 +1,21 @@
+class cat:
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Meow")   
+class Dog:
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Bark")
+cat1 = cat("Dodo", 2.5)
+dog1 = Dog("Kedo", 8)        
+for animal in (cat1, dog1):
+    animal.info()
+    animal.make_sound()
