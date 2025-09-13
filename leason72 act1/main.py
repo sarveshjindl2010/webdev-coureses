40d901cc956530b8587fde35861e018c4ffb50e7
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-09-13 20:28:08 +0530

    sgdVGVS

---
+++ leason72 act1/main.py
@@ -0,0 +1,3 @@
+file = open('BMW is a beast car')
+print(file.read())
+file.close()
---
+++ test py/main.py
@@ -0,0 +1,9 @@
+def sum(x, y):
+    print("The sum of x and y is:", x + y)
+def sub(x, z):
+    print("The subtraction of x and z is:", x - z)
+def Multiply(x, b):
+    print("The multiplication of x and y is:", x * b)    
+sum = sum(4, 7)
+sub = sub(5, 2)
+mul = Multiply(3, 6)
