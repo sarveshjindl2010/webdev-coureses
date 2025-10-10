974d7a171c6df75342ca0c7f601e602010110d17
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-10-10 20:25:07 +0530

    fndthbetydbw

---
+++ leason79 act1/main.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    literation = 0
+    print("The total number entered is",n)
+    literation+=2
+    print("total literation done by computer is", literation,"\n")
+printnumber(50)
+printnumber(400)
+print("\n with any 'n the time taken by the computer will not change")
+#order of 2 compexity
