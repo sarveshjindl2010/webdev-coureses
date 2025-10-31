d31010accec99c151d31ad8d86de6cb9138d0e39
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-10-31 20:25:07 +0530

    aXDdC DWVEFCQDFC

---
+++ leason83 act1/main.py
@@ -0,0 +1,9 @@
+num1 = 12
+num2 = 10
+print('num1 & num2 are:', num1, num2)
+print('num1 | num2 are:', num1 |num2)
+print('num1 ^ num2 are:', num1 ^ num2)
+print('num1 << num2 are:', num1 << num2)
+print('num1 >> num2 are:', num1 >> num2)
+print('~num1 are:', ~num1)
+print('~num2 are:', ~num1)
