0506366c64026852a744420a5b25d756be22de1b
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-11-08 20:23:11 +0530

    asfdSCC

---
+++ leason85 act2/main.py
@@ -0,0 +1,12 @@
+def OddOccuring(arr):
+    res = 0
+    for element in arr:
+        res ^= element
+    return res
+arr = []
+n = int(input("Enter array size:" ))
+while(n):
+    num = int(input("Enter number: "))
+    arr.append(num)
+    n-= 1
+print("Odd Occuring number is: ", OddOccuring(arr))
