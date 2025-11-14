1d0ab966d8e63c4bf7f359ae1553c4771ccba0bb
sarveshjindal2010 <sarveshjindal2010@gmail.com>
2025-11-14 20:28:16 +0530

    rgzfsvzsfcv

---
+++ leason86 act2/main.py
@@ -0,0 +1,15 @@
+def powerOf4(number):
+    count = 0
+    if (number & (~(number - 1))):
+        while(number > 1):
+            number >>= 1
+            count += 1
+        if(count %2 == 0):
+            return True
+        else:
+            return False
+number = int(input("Enter the number: "))
+if(powerOf4(number)):
+    print(number, "is a power of 4")
+else:
+    print(number, "isn't a power of 4")
